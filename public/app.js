9d5fd69867f5c2013f163dbfef25ff74fc6c8fb5
Fil <vtl.filimonov@gmail.com>
2017-07-01 18:27:39 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,18 +1,23 @@
-function add_to_cart(id)
-{
-	alert('You added pizza with id: ' + id);
-}
-
-
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 	alert(x);
 	
+}
+
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+
+	x = x * 1 + 1;
+	
+	window.localStorage.setItem(key, x);
 }
 
