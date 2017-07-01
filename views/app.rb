c71b852eab892084fa746a6c7ca3426193f11002
Fil <vtl.filimonov@gmail.com>
2017-07-01 18:46:52 +0300

    Update images

---
+++ views/app.rb
@@ -0,0 +1,29 @@
+hh = {}
+
+loop do
+	
+	print 'Enter product id: '
+	id = gets.chomp
+
+	print 'Enter amount (how much items you want to order): ' 
+	n = gets.chomp.to_i
+
+	x = hh[id].to_i # read hash value (o if it's not exist)
+	x = x + n 		
+
+	hh[id] = x
+
+	puts hh.inspect
+
+	# calculate total number of items in cart
+	total = 0
+	hh.each do |key,value|
+		total = total + value
+	end
+
+	# puts total variable
+	puts "Total items in cart: #{total}"
+
+	puts '====================================='
+
+end
