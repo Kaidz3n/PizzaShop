077fad480290af4c6dc8e427513a3e35580b6da8
Fil <vtl.filimonov@gmail.com>
2017-07-02 13:19:14 +0300

    Add cart_get_numbers_of_items function

--- public/app.js
+++ public/app.js
@@ -19,5 +19,25 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	
 	window.localStorage.setItem(key, x);
+
+// вывод количества item'ов в корзине
+	alert('Items in your cart: ' + cart_get_numbers_of_items());
 }
 
+function cart_get_numbers_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в руби hh[key] = x
+		
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+}
