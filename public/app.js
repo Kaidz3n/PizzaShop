419e370dafece827f903277bb1cc5889728c0ef7
Fil <vtl.filimonov@gmail.com>
2017-07-04 11:26:51 +0300

    Replace cart contents on cancel

--- public/app.js
+++ public/app.js
@@ -76,8 +76,10 @@ function cancel_order()
 {
 	window.localStorage.clear();
 
-		update_orders_input();
-		update_orders_button();
+	update_orders_input();
+	update_orders_button();
+
+	$('#cart').text('Your cart is now empty');
 
 	return false;
 }
